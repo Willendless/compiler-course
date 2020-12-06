3ada35a5bb24c49a7d80b297bc609e77b230eb02
Willendless <317500141@qq.com>
2020-12-06 18:53:29 +0800

    chore: basic arch of llgen with bug

--- src/llgen/llgen.c
+++ src/llgen/llgen.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "../lib/set.h"
 #include "../lib/table.h"
 #include "../lib/darray.h"
@@ -20,6 +21,9 @@ static unsigned hash_str(const void *key);
 static bool cmp_int(const void *a, const void *b);
 static unsigned hash_int(const void *key);
 
+static void output_table(void);
+static void output_productions(void);
+
 // in, out
 FILE* in;
 FILE* out;
@@ -34,12 +38,15 @@ DArray *first;
 DArray *follow;
 // buf
 char buf[1024];
-static const char epsilon[] = "epsilon";
 // output table
 int **table;
+int x_end;
+int y_end;
 
 void init(void) {
+    // index -> grammar
     grammar_arr = DArray_init(70, 100);
+    // grammar -> index
     grammar_table = Table_init(100, cmp_str, hash_str);
     production_arr = DArray_init(70, 100);
     first = DArray_init(sizeof(Set), 70);
@@ -61,7 +68,9 @@ void gen_table(void) {
     gen_first_sets();
     gen_follow_sets();
     // TODO(ljr): table generatation algo
-    table = (int **)malloc(sizeof(non_index-1)*sizeof(grammar_arr->end-non_index+1));
+    table = (int **)calloc(1, sizeof(non_index-1)*sizeof(grammar_arr->end-non_index+1));
+    x_end = grammar_arr->end - non_index + 1;
+    y_end = non_index;
     // for each nonterminal
     for (i = non_index; i <= grammar_arr->end; ++i) {
         char *grammar = DArray_get(grammar_arr, i);
@@ -82,16 +91,17 @@ static void gen_first_sets(void) {
     bool flag = TRUE;
     int prev_cnt, cur_cnt;
     int i, j, k;
-    Set *s_first;
     Set *cur;
     // first set for terminal
     for (i = 1; i < non_index; ++i) {
-        DArray_set(first, i, Set_init(5, cmp_str, hash_str)); 
-        Set_put(DArray_get(first, i), i);
+        // index -> index
+        DArray_set(first, i, Set_init(5, cmp_int, hash_int)); 
+        Set_put(DArray_get(first, i), (int *)i);
     }
     // first set for nonterminal
     for (i = non_index; i <= grammar_arr->end; ++i) {
-        DArray_set(first, i, Set_init(5, cmp_str, hash_str)); 
+        // index -> index
+        DArray_set(first, i, Set_init(5, cmp_int, hash_int)); 
     }
     // fixed-point
     while (flag) {
@@ -105,14 +115,12 @@ static void gen_first_sets(void) {
             // for each production on one head
             for (j = 0; j <= productions->end; ++j) {
                 DArray *production = DArray_get(productions, j);
-                char *product0 = DArray_get(production, 0);
-                Set *pre = get_grammar_set(first, product0);
+                Set *pre = DArray_get(first, (int)DArray_get(production, 0));
                 // if s_head doesn't include epsilon, we cannot add it here
                 union_without_epsilon(s_head, pre);
                 // for other production union if epsilon exists in previous set
                 for (k = 1; k <= production->end && Set_member(pre, "epsilon"); ++k) {
-                    char *productk = DArray_get(production, k);
-                    cur = get_grammar_set(first, productk); 
+                    cur = DArray_get(first, DArray_get(production, k));
                     union_without_epsilon(s_head, cur);
                 }
                 if (k > production->end && Set_member(cur, "epsilon")) {
@@ -139,7 +147,6 @@ static void union_without_epsilon(Set *dest, Set *src) {
     } else {
         Set_union(dest, src);
         Set_remove(dest, "epsilon");
-
     }
 }
 
@@ -158,7 +165,59 @@ error:
 }
 
 static void gen_follow_sets(void) {
-
+    int i, j, k;
+    bool flag = TRUE;
+    int prev_cnt;
+    int cur_cnt;
+    Set *record = Set_init(100, cmp_int, hash_int);
+    // init follow set
+    for (i = non_index; i <= grammar_arr->end; ++i) {
+        DArray_set(follow, i, Set_init(100, cmp_int, hash_int));
+    }
+    // put eof to follow(start)
+    Set_put(DArray_get(follow, non_index), Table_get(grammar_table, "eof"));
+    //fixed-point
+    while (flag) {
+        // for each production head
+        for (i = non_index; i <= production_arr->end; ++i) {
+            DArray *productions = DArray_get(production_arr, i);
+            // for each production with the same head
+            for (j = 0; j <= productions->end; ++j) {
+                // init record set to follow[head]
+                Set_clear(record);
+                check(Set_length(record) == 0, "Failed to clear set record.");
+                Set_union(record, DArray_get(follow, i));
+                // get cur production
+                DArray *production = DArray_get(productions, j);
+                // for each grammar
+                for (k = production->end; k >= 0; --k) {
+                    // index of grammar in product[k]
+                    int index = DArray_get(production, k);
+                    Set *cur = DArray_get(follow, index);
+                    // record prev_cnt to check if cur changed
+                    prev_cnt = Set_length(cur);
+                    Set_union(cur, record);
+                    // product[k] can be epsilon, then follow[product[k]] can be in record
+                    // otherwise, record can only contains first[product[k]]
+                    if (Set_member(DArray_get(first, index), "epsilon")) {
+                        Set_union(record, DArray_get(first, index));
+                        Set_remove(record, "epsilon");
+                    } else {
+                        Set_clear(record);
+                        check(Set_length(record) == 0, "Failed to clear set record.");
+                        Set_union(record, DArray_get(first, index));
+                    }
+                    cur_cnt = Set_length(cur);
+                    check(cur_cnt >= prev_cnt, "Follow set elements removed.");
+                    if (cur_cnt > prev_cnt) {
+                        flag = TRUE;
+                    }
+                }
+            }
+        }
+    }
+error:
+    return;
 }
 
 static inline void handle_input(void) {
@@ -193,28 +252,27 @@ static inline void handle_production() {
     int index;
     DArray *productions;
     DArray *production;
-    char *tmp;
 
     while (fscanf(in, "%s", buf) != EOF) {
         // read production head which must be nonterminal
         fscanf(in, "%s", buf);
         // get head's index
         index = Table_get(grammar_table, buf);
-        check(index != NULL, "Unknown to get nonterminal");
+        check(index != NULL, "Unknown to get nonterminal.");
         check(strcmp(buf, "->") == 0, "Invalid production format.");
         productions = DArray_init(sizeof(DArray), 5);
         DArray_set(production_arr, index, productions);
         // handle production body
         while (strcmp(buf, "$")) {
             // init production
-            production = DArray_new(productions);
+            production = DArray_init(sizeof(int), 10);
             DArray_push(productions, production);
             // read each grammar
             fscanf(in, "%s", buf);
             while (strcmp(buf, "!") && strcmp(buf, "$")) {
-                tmp = DArray_new(production);
-                strcpy(tmp, buf);
-                DArray_push(production, tmp);
+                index = Table_get(grammar_table, buf);
+                check(index != NULL, "Unknown to get grammar.");
+                DArray_push(production, (void *)index);
                 fscanf(in, "%s", buf);
             }
         }
@@ -225,7 +283,37 @@ error:
 }
 
 static void handle_output(void) {
+    fprintf(out, "#include <stdio.h>\n");
+    fprintf(out, "#include \"parser.h\"\n");
+    output_table();
+    output_productions();
+}
 
+static void output_table() {
+    int i, j;
+    // output ll parser table
+    fprintf(out, "const int[][] LL_PARSE_TABLE = [\n");
+
+    // for each nonterminal
+    for (i = 0; i < x_end; i++) {
+        fprintf(out, "    [_");
+        // for each terminal
+        for (j = 1; j < y_end; ++j) {
+            char s[10];
+            sprintf(s, "%d", table[i][j]);
+            fprintf(out, ", %s", s);
+        }
+        fprintf(out, "]\n");
+    }
+
+    fprintf(out, "];\n");
+}
+
+static void output_productions() {
+    fprintf(out, "\n");
+    fprintf(out, "\n");
+
+    fprintf(out, "const int** productions");
 }
 
 static bool cmp_str(const void *a, const void *b) {
@@ -247,6 +335,6 @@ static bool cmp_int(const void *a, const void *b) {
     return a != b;
 }
 
-static unsigned hash_init(const void *key) {
+static unsigned hash_int(const void *key) {
     return (unsigned long)key >> 2;
 }
---
+++ src/llgen/llgen.h
@@ -0,0 +1,6 @@
+#ifndef _LLGEN_H_
+#define _LLGEN_H_
+
+void run(FILE *in, FILE* out);
+
+#endif
